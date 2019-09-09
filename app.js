7c85bbff61066f2ac851141b52d1d72eb7e06894
Flawid D'Souza <me@flawiddsouza.com>
2019-09-09 16:34:22 +0530

    Adds support for urlencoded POST

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 var auth = require('./routes/auth')
 app.use('/', auth)
